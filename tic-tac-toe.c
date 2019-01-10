dab3c45807a169f68efbe7f587b098bb8d2352fe
Milo MacPhail <milomacphail@gmail.com>
2019-01-10 23:54:03 +0000

    Added counter

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -6,14 +6,18 @@ void drawBoard()
 {
   char board[9];
   int i;
+  int xCount = 0;
+  int oCount = 0;
 
   for( i = 0; i < 9; i++){
     board[i]= ' ';
     if (i % 2 == 1)
     {
         board[i] = 'o';
+        oCount ++;
     } else {
         board[i] = 'x';
+        xCount ++;
     }
 
     }
@@ -22,6 +26,8 @@ void drawBoard()
    printf(" %c | %c | %c \n", board[3], board[4], board[5]);
    printf("---+---+---\n");
    printf(" %c | %c | %c \n", board[0], board[1], board[2]);
+   printf("X Count:%i\n", xCount);
+   printf("O Count:%i\n", oCount);
   }
 
 int main()
